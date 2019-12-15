88e6a793c12dc89daf6961d7305db284b9f32f4f
Ellmoo13 <august.miszczak@wp.pl>
2019-12-15 18:36:27 +0100

    23

---
+++ Section_02/BullCowsGame/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	//introduse the game 
+	constexpr int WORLD_LENGTH = 9;
+
+	cout << "Welcone to Bulls and Cows";
+	cout << "Can you guess the " << WORLD_LENGTH;
+	cout << " Letter isogram I'm thinking of \n";
+	cout << endl;
+
+	//get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	//repeat the guess back to them
+	cout << "You quess wa: " << Guess << endl;
+
+	return 0;
+}
